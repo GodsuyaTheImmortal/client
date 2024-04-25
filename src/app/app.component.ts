816ea5538263e42226cf772c04c314cfef9c3559
GodsuyaTheImmortal <lawtq-wp20@student.tarc.edu.my>
2024-04-25 20:55:59 +0800

    End of Section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'Dating App';
+
+  users: any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    });
+  }
 }
