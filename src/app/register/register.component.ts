f62431df87c42796ca53222a5db273cf6d9b6832
GodsuyaTheImmortal <lawtq-wp20@student.tarc.edu.my>
2024-11-26 21:59:16 +0800

    Problem With Member Not Appearing On List

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,86 +1,69 @@
-import { Component, Output, EventEmitter, inject, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { TextInputComponent } from "../_forms/text-input/text-input.component";
+import { Component, OnInit, inject, output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { ToastrService } from 'ngx-toastr';
+import { TextInputComponent } from "../_forms/text-input/text-input.component";
 import { DatePickerComponent } from '../_forms/date-picker/date-picker.component';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-register',
-  standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, TextInputComponent, DatePickerComponent],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+    selector: 'app-register',
+    standalone: true,
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.scss',
+    imports: [ReactiveFormsModule, TextInputComponent, DatePickerComponent]
 })
-export class RegisterComponent implements OnInit{
-
-  @Output() cancelRegister = new EventEmitter();
-
-  private accountService: AccountService = inject(AccountService);
-  private toastr: ToastrService = inject(ToastrService);
-  private fb: FormBuilder = inject(FormBuilder);
-  private router: Router = inject(Router);
-
-  maxDate: Date = new Date();
-  
-  model: any = {};
-
+export class RegisterComponent implements OnInit {
+  private accountService = inject(AccountService);
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  cancelRegister = output<boolean>();
   registerForm: FormGroup = new FormGroup({});
-
-  validationError: string[] | undefined;
+  maxDate = new Date();
+  validationErrors: string[] | undefined;
 
   ngOnInit(): void {
     this.initializeForm();
-    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18)
   }
 
   initializeForm() {
-    this.registerForm =this.fb.group({
+    this.registerForm = this.fb.group({
       gender: ['male'],
       username: ['', Validators.required],
       knownAs: ['', Validators.required],
       dateOfBirth: ['', Validators.required],
       city: ['', Validators.required],
       country: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(50)]],
+      password: ['', [Validators.required, Validators.minLength(4), 
+          Validators.maxLength(8)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]],
     });
-
     this.registerForm.controls['password'].valueChanges.subscribe({
       next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
     })
   }
 
-  matchValues(matchTo: string): ValidatorFn{
+  matchValues(matchTo: string): ValidatorFn {
     return (control: AbstractControl) => {
-      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+      return control.value === control.parent?.get(matchTo)?.value ? null : {isMatching: true}
     }
   }
 
-  register(){
-
-    const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
-    const values = {...this.registerForm.value, dob};
-
-    this.accountService.register(values).subscribe({
-      next: () => {
-        this.router.navigateByUrl('/member');
-      },
-      error: error => {
-        this.validationError = error;
-      }
+  register() {
+    const dob = this.getDateOnly(this.registerForm.get('dateOfBirth')?.value);
+    this.registerForm.patchValue({dateOfBirth: dob});
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: _ => this.router.navigateByUrl('/members') ,
+      error: error => this.validationErrors = error
     })
   }
 
-  cancel(){
+  cancel() {
     this.cancelRegister.emit(false);
   }
 
-  private getDateOnly(dob: string | undefined){
-    if(!dob) return;
-    let theDob = new Date(dob);
-    return new Date(theDob.setMinutes(theDob.getMinutes() - theDob.getTimezoneOffset())).toISOString().slice(0, 10);
-  }
+  private getDateOnly(dob: string | undefined) {
+    if (!dob) return;
+    return new Date(dob).toISOString().slice(0, 10);
+  } 
 }
